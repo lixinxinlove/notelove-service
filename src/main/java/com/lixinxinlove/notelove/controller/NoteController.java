b8cd565d9552e67cac116ab2ea3fefd34c6ca125
lixinxin <895330766@qq.com>
2019-02-18 17:50:03 +0800

    保存数据

--- src/main/java/com/lixinxinlove/notelove/controller/NoteController.java
+++ src/main/java/com/lixinxinlove/notelove/controller/NoteController.java
@@ -1,9 +1,13 @@
 package com.lixinxinlove.notelove.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lixinxinlove.notelove.dataobject.NoteDO;
+import com.lixinxinlove.notelove.response.CommonReturnType;
+import com.lixinxinlove.notelove.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("note")
 @RestController
@@ -11,7 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
 
 
+    @Autowired
+    private NoteService noteService;
 
-
+    @RequestMapping(value = "/save", method = {RequestMethod.POST})
+    public CommonReturnType login(@RequestBody List<NoteDO> noteDOList) {
+        //TODO 修改信息
+        int i = noteService.saveNoteList(noteDOList);
+        if (i > 0) {
+            return CommonReturnType.create("");
+        } else {
+            return CommonReturnType.error("");
+        }
+    }
 
 }
