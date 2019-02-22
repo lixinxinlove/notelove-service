56b478c939782daec8b4561d118940942fd31a58
lixinxin <895330766@qq.com>
2019-02-22 18:49:02 +0800

    添加阿里云数据

--- src/main/java/com/lixinxinlove/notelove/NoteloveApplication.java
+++ src/main/java/com/lixinxinlove/notelove/NoteloveApplication.java
@@ -3,13 +3,23 @@ package com.lixinxinlove.notelove;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.lixinxinlove.notelove.dao")
-public class NoteloveApplication {
+public class NoteloveApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NoteloveApplication.class, args);
     }
+
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
+
 
 }
